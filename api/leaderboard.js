ba5918550a7f2fce7ecc330f8bd534589b7239a1
ElDoktor <rustamahatov@Rustams-MacBook-Air.local>
2026-01-29 22:23:34 +0300

    chore: bump version [auto]

--- api/leaderboard.js
+++ api/leaderboard.js
@@ -12,7 +12,7 @@ import { Redis } from '@upstash/redis';
 
 const LEADERBOARD_KEY = 'leaderboard:results';
 const MAX_RESULTS = 1000;
-const API_VERSION = '1.0.80';
+const API_VERSION = '1.0.81';
 
 function formatAddress(address) {
   if (!address) return 'Guest';
