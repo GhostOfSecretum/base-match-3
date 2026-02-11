534eb4757b63bf3195b12d06d707835533bcf93d
ElDoktor <rustamahatov@Rustams-MacBook-Air.local>
2026-02-11 13:57:36 +0530

    chore: bump version [auto]

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // НЕМЕДЛЕННОЕ ЛОГИРОВАНИЕ - должно выполниться первым
-const APP_VERSION = '1.0.110';
+const APP_VERSION = '1.0.111';
 console.log('=== SCRIPT.JS VERSION', APP_VERSION, '===');
 console.log('Timestamp:', new Date().toISOString());
 
